3c2dd1c5ac38e3669b62d87f018e5282d1fc66d8
Dawid Wysocki <tortillazhawaii@gmail.com>
2024-02-21 12:21:02 +0100

    Initial commit

---
+++ OrleansWorkbench.Etcd/Program.cs
@@ -0,0 +1,8 @@
+﻿using dotnet_etcd;
+
+var etcdClient = new EtcdClient("http://localhost:2379");
+
+await etcdClient.PutAsync("foo/bar", "baz");
+var response = await etcdClient.GetValAsync("foo/bar");
+
+Console.WriteLine(response);
